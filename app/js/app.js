95426098d99804e9c97da1471a7681747db1eb0f
Mikolaj <mikos_1997@wp.pl>
2022-01-12 22:54:55 +0100

    (Schedule) added loopa to schedule img

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,12 @@
 // // Import vendor jQuery plugin example
 // import '~/app/libs/mmenu/dist/mmenu.js'
 document.addEventListener('DOMContentLoaded', () => {
+    const loopa = document.getElementById('loopa');
+    document.getElementById('schedule-image').addEventListener('mouseleave', event => {
+        loopa.style.display = 'none';
+    });
+    document.getElementById('schedule-image').addEventListener('mouseover', event => {
+        loopa.style.display = 'block';
+    });
 
 })
